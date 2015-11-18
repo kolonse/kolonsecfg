c4d71940b80c4434ed04d38399c8c94b80c5a680
kolonse <KolonseTiger@gmail.com>
2015-11-18 18:50:06 +0800

    将文件进行分组

--- node.go
+++ node.go
@@ -1,73 +1,28 @@
 package cfg
 
-import (
-	"errors"
-	"reflect"
-)
-
-type Value struct {
-	Bool      bool              //bool类型
-	Int       int64             //整形
-	String    string            // 字符串类型
-	Double    float64           // 浮点数类型
-	Array     []*Value          // 数组类型 []
-	Object    map[string]*Value //对象类型 {}
-	ValueType int               // 值类型
-}
-
-func NewValue(valueType int, v interface{}) *Value {
-	value := &Value{
-		ValueType: valueType,
-	}
-	t := reflect.TypeOf(v).Kind()
-	switch valueType {
-	case BOOL:
-		if t == reflect.Bool {
-			value.Bool = v.(bool)
-		} else {
-			panic(errors.New("数值类型非 bool 类型,而是 " + reflect.TypeOf(v).String()))
-		}
-	case INT:
-		if t >= reflect.Int || t <= reflect.Uint64 { // 只要是整数全部转为 int64 避免麻烦
-			value.Int = v.(int64)
-		} else {
-			panic(errors.New("数值类型非 int 类型,而是 " + reflect.TypeOf(v).String()))
-		}
-	case STRING:
-		if t == reflect.String {
-			value.String = v.(string)
-		} else {
-			panic(errors.New("数值类型非 string 类型,而是 " + reflect.TypeOf(v).String()))
-		}
-	case DOUBLE:
-		if t >= reflect.Float32 || t <= reflect.Float64 {
-			value.Double = v.(float64)
-		} else {
-			panic(errors.New("数值类型非 float 类型,而是 " + reflect.TypeOf(v).String()))
-		}
-	case ARRAY:
-		if t == reflect.Array {
-			value.Array = append(value.Array, v.([]*Value)...)
-		} else {
-			panic(errors.New("数值类型非 array 类型,而是 " + reflect.TypeOf(v).String()))
-		}
-	case OBJECT:
-		if t == reflect.Map { // 只要是整数全部转为 int64 避免麻烦
-			//			value.Int = v.(int64)
-			value.Object = make(map[string]*Value)
-			for key, vlu := range v.(map[string]*Value) {
-				value.Object[key] = vlu
-			}
-		} else {
-			panic(errors.New("数值类型非 object 类型,而是 " + reflect.TypeOf(v).String()))
-		}
-	}
-	return value
-}
-
 type Node struct {
 	key     string
 	value   *Value
 	farther *Node
 	child   []*Node
+}
+
+func (n *Node) SetFarther(farther *Node) {
+	n.farther = farther
+}
+
+func (n *Node) AddChild(child *Node) {
+	n.child = append(n.child, child)
+	child.SetFarther(n)
+}
+
+func (n *Node) AddChilds(childs []*Node) {
+	n.child = append(n.child, childs...)
+	for _, i := range childs {
+		childs[i].SetFarther(n)
+	}
+}
+
+func NewNode() {
+	return Node{}
 }
---
+++ value.go
@@ -0,0 +1,67 @@
+// value
+package cfg
+
+import (
+	"errors"
+	"reflect"
+)
+
+type Value struct {
+	Bool      bool              //bool类型
+	Int       int64             //整形
+	String    string            // 字符串类型
+	Double    float64           // 浮点数类型
+	Array     []*Value          // 数组类型 []
+	Object    map[string]*Value //对象类型 {}
+	ValueType int               // 值类型
+}
+
+func NewValue(valueType int, v interface{}) *Value {
+	value := &Value{
+		ValueType: valueType,
+	}
+	t := reflect.TypeOf(v).Kind()
+	switch valueType {
+	case BOOL:
+		if t == reflect.Bool {
+			value.Bool = v.(bool)
+		} else {
+			panic(errors.New("数值类型非 bool 类型,而是 " + reflect.TypeOf(v).String()))
+		}
+	case INT:
+		if t >= reflect.Int || t <= reflect.Uint64 { // 只要是整数全部转为 int64 避免麻烦
+			value.Int = v.(int64)
+		} else {
+			panic(errors.New("数值类型非 int 类型,而是 " + reflect.TypeOf(v).String()))
+		}
+	case STRING:
+		if t == reflect.String {
+			value.String = v.(string)
+		} else {
+			panic(errors.New("数值类型非 string 类型,而是 " + reflect.TypeOf(v).String()))
+		}
+	case DOUBLE:
+		if t >= reflect.Float32 || t <= reflect.Float64 {
+			value.Double = v.(float64)
+		} else {
+			panic(errors.New("数值类型非 float 类型,而是 " + reflect.TypeOf(v).String()))
+		}
+	case ARRAY:
+		if t == reflect.Array {
+			value.Array = append(value.Array, v.([]*Value)...)
+		} else {
+			panic(errors.New("数值类型非 array 类型,而是 " + reflect.TypeOf(v).String()))
+		}
+	case OBJECT:
+		if t == reflect.Map { // 只要是整数全部转为 int64 避免麻烦
+			//			value.Int = v.(int64)
+			value.Object = make(map[string]*Value)
+			for key, vlu := range v.(map[string]*Value) {
+				value.Object[key] = vlu
+			}
+		} else {
+			panic(errors.New("数值类型非 object 类型,而是 " + reflect.TypeOf(v).String()))
+		}
+	}
+	return value
+}
